3f1f9b5b7225df874d03731edfbd88b4ae1e1cde
Urmat Bekboev <bek.urmat@gmail.com>
2019-02-12 06:25:32 +0600

    code refactor

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -4,17 +4,4 @@ const instance = axios.create({
     baseURL: 'https://restcountries.eu/'
 });
 
-// instance.interceptors.request.use(req => {
-//     console.log('[In request interceptor]' ,req);
-//     return req;
-// });
-
-// instance.interceptors.response.use(res => {
-//    console.log('[In response interceptor]' ,res);
-//    return res;
-// }, error => {
-//     console.log('[In response error intercetor]', error);
-//     throw error;
-// });
-
 export default instance;
